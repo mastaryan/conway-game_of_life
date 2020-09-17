7ff7708774f90cc3f885906841397793624f03cd
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-09-17 15:25:59 -0400

    simplify board controls

--- src/components/BoardControls.js
+++ src/components/BoardControls.js
@@ -1,60 +1,17 @@
 import React, { Component } from 'react';
 
 class BoardControls extends Component {
-  handleColumnChange = (e) => {
-
-  }
-
-  handleRowChange = (e) => {
-    
-  }
-
-  startGame = () => {
-
-  }
-
-  stopGame = () => {
-
-  }
-
-  clearGrid = () => {
-
-  }
-
-  randomConfig = () => {
-
-  }
-
   render() {
     return (
       <div>
         <div>
-          <button onClick={this.startGame}>Start</button>
-          <button onClick={this.stopGame}>Stop</button>
-          <button onClick={this.clearGrid}>Clear</button>
-        </div>
-        <div>
-          <button onClick={this.randomConfig}>Random</button>
-        </div>
-        <div>
-          <div>
-            <label>Columns</label>
-            <input
-              type="text"
-              value={this.props.size[0]}
-              onChange={this.handleColumnChange} />
-          </div>
-          <div>
-            <label>Rows</label>
-            <input
-              type="text"
-              value={this.props.size[1]}
-              onChange={this.handleRowChange} />
-          </div>
+          <button onClick={this.props.startGame}>Start</button>
+          <button onClick={this.props.stopGame}>Stop</button>
+          <button onClick={this.props.clearGrid}>Clear</button>
         </div>
       </div>
     );
-  }
-}
+  };
+};
 
 export default BoardControls;
