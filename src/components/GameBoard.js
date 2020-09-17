e4adc3795e75ea7d109aed5dd8116d271059f4e7
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-09-17 15:59:47 -0400

    cleaned up gameboard. Adding components to a new components file, grid.

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,34 +1,31 @@
 import React, { Component } from 'react';
 import Grid from './Grid';
-import BoardControls from './BoardControls';
 import Rules from './Rules';
 import { RulesButton } from './Styles';
 
 class GameBoard extends Component {
   state = {
     open: false,
-    size: [25, 25]
-  }
+  };
 
   openModal = () => {
-    this.setState({ open: true })
-  }
+    this.setState({ open: true });
+  };
 
   closeModal = () => {
-    this.setState({ open: false })
-  }
+    this.setState({ open: false });
+  };
 
   render() {
     return (
       <div>
         <h1>Conway's Game of Life</h1>
         <RulesButton onClick={this.openModal}>Rules</RulesButton>
-        <Grid size={this.state.size} />
-        <BoardControls size={this.state.size} />
+        <Grid />
         <Rules open={this.state.open} closeModal={this.closeModal} />
       </div>
     );
-  }
-}
+  };
+};
 
 export default GameBoard;
